25c9c3db9a39bc5d953cb49edacdcc432a9e9606
nc0d3 <65702184+NC0D3@users.noreply.github.com>
2025-04-07 00:46:12 -0600

    Create sketch.js

---
+++ js/sketch.js
@@ -0,0 +1,32 @@
+const canvas = document.getElementById("flockCanvas");
+const ctx = canvas.getContext("2d");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+window.addEventListener("resize", () => {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+});
+
+
+const flock = [];
+
+let alignSlider, cohesionSlider, separationSlider;
+
+function setup() {
+    createCanvas(canvas.width,canvas.height);
+    colorMode(HSB, 360, 100, 100);
+    for (let i = 0; i < 300; i++) {
+        flock.push(new Boid());
+    }
+}
+
+function draw() {
+    background(0);
+    for (let boid of flock) {
+        boid.edges();
+        boid.flock(flock);
+        boid.update();
+        boid.show();
+    }
+}
