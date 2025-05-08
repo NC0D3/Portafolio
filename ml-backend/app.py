2ca88b3164f34592445c83c0f61a5c695fa525a4
nc0d3 <65702184+NC0D3@users.noreply.github.com>
2025-05-08 00:39:46 -0600

    Create app.py

---
+++ ml-backend/app.py
@@ -0,0 +1,11 @@
+from flask import Flask, request, send_file
+
+app = Flask(__name__)
+
+@app.route("/process", methods=["POST"])
+def process():
+    return "pollo autorizao para rebotar en estado cuantico"
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
